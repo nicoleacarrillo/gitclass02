c6fa20420118e3e65c60f3dcfae7cd0570f35725
nicoleacarrillo <nicoleacarrillo0532@gmail.com>
2025-02-17 22:15:11 -0500

    fourth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,10 +1,7 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("Version 1");
+        System.out.println("Done with Version 4");
 
-        System.out.println("Version 2");
-
-        System.out.println("Version 3");
     }
 }
